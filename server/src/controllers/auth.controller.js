0a54cd1b36de8af1281ed797ab64d7a863c0dfbc
HimanshuDobariya <himanshudobariya46@gmail.com>
2025-02-05 17:27:43 +0530

    add route to verify email

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -43,4 +43,36 @@ const signup = async (req, res) => {
   }
 };
 
-export { signup };
+const verifyEmail = async (req, res) => {
+  try {
+    const { email, code } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+    if (code !== user.verificationCode) {
+      return res.status(400).json({ message: "Invalid verification code" });
+    }
+    if (user.verificationCodeExpireAt < Date.now()) {
+      return res
+        .status(400)
+        .json({ message: "Verification code is expired. Send it again." });
+    }
+
+    user.isVerified = true;
+    user.verificationCode = undefined;
+    user.verificationCodeExpireAt = undefined;
+
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully." });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Server error.", error: error.message });
+  }
+};
+
+export { signup, verifyEmail };
