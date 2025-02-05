c39e3b0f9e97f9c32d537ad1b0ab84a459b65ae6
HimanshuDobariya <himanshudobariya46@gmail.com>
2025-02-05 09:12:27 +0530

    setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@material-tailwind/react";
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+    </div>
+  );
+};
+export default App;
