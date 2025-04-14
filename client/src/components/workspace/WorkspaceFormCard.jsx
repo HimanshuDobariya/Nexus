f17b0413974a6d187b3bfc6b12db881189dfc124
HimanshuDobariya <himanshudobariya46@gmail.com>
2025-04-14 12:04:03 +0530

    add logout button in create workspace page

--- client/src/components/workspace/WorkspaceFormCard.jsx
+++ client/src/components/workspace/WorkspaceFormCard.jsx
@@ -1,30 +1,46 @@
 import DottedSeperator from "@/components/common/DottedSeperator";
 import WorkspaceForm from "./WorkspaceForm";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+import { useAuthStore } from "@/store/authStore";
 
 const WorkspaceFormCard = () => {
+  const { logout } = useAuthStore();
   return (
-    <div className="flex items-center justify-center min-h-screen bg-neutral-100 px-4">
-      <div className=" p-4 w-full  lg:max-w-screen-md mx-auto rounded-md">
-        <Card className="w-full shadow-none border-none">
-          <CardHeader className="flex p-7">
-            <CardTitle className="text-xl font-bold">
-              Create a new workspace
-            </CardTitle>
-          </CardHeader>
-          <div className="px-7">
-            <DottedSeperator />
-          </div>
-          <CardContent className="p-7">
-            <WorkspaceForm
-              mode="create"
-              initialData={null}
-              setOpen={() => {
-                return null;
-              }}
-            />
-          </CardContent>
-        </Card>
+    <div className="min-h-screen bg-neutral-100 p-4">
+      <Button
+        className="ml-auto flex items-center"
+        variant="destructive"
+        onClick={() => {
+          logout();
+          localStorage.removeItem("activeWorkspaceId");
+        }}
+      >
+        <LogOut className="!size-4 mr-1" /> Log out
+      </Button>
+      <div className="flex items-center justify-center ">
+        <div className=" p-4 w-full  lg:max-w-screen-md mx-auto rounded-md">
+          <Card className="w-full shadow-none border-none">
+            <CardHeader className="flex p-7">
+              <CardTitle className="text-xl font-bold">
+                Create a new workspace
+              </CardTitle>
+            </CardHeader>
+            <div className="px-7">
+              <DottedSeperator />
+            </div>
+            <CardContent className="p-7">
+              <WorkspaceForm
+                mode="create"
+                initialData={null}
+                setOpen={() => {
+                  return null;
+                }}
+              />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
